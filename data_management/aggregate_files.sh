ca0e0154ab49ae12c1452dc7fbb8c37f15eb4c9d
hibikaze-git <hrk.ymgch@gmail.com>
2024-03-21 18:11:59 +0900

    ファイルの統合時に引数を指定

--- data_management/aggregate_files.sh
+++ data_management/aggregate_files.sh
@@ -1,11 +1,22 @@
 #!/bin/sh
 
+#bash aggregate_files.sh output/filtering output/filtered result.filtering.jsonl
+#bash aggregate_files.sh output/dedup output/deduped result.dedup.jsonl
+
+if [ "$#" -ne 3 ]; then
+    echo "Usage: $0 <input_directory> <output_directory> <jsonl_file>"
+    exit 1
+fi
+
+input_directory="$1"
+output_directory="$2"
+jsonl_file="$3"
 counter=1
 
-# outputディレクトリ内のすべてのサブディレクトリに対して処理を行う
-find output/filtering/* -type f | while read filename; do
+# input_directory内のすべてのサブディレクトリに対して処理を行う
+find "$input_directory"/* -type f -name "$jsonl_file" | while read filename; do
     # ファイルを移動して名前を変更
-    cp "$filename" "output/filtered/$counter.jsonl"
+    cp "$filename" "$output_directory/$counter.jsonl"
     # カウンターをインクリメント
     ((counter++))
 done
