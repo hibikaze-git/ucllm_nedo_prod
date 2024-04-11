5fee3e9416f7b7c71f72854e6a77711215fc93bf
hibikaze-git <hrk.ymgch@gmail.com>
2024-04-11 09:02:13 +0900

    データアップロード用のコードを追加

---
+++ data_management/load_dataset_local/upload_hf_hub.py
@@ -0,0 +1,22 @@
+"""
+jsonlをHFにアップロード
+python upload_hf_hub.py ./data/0313wiki.jsonl hibikaze/upload_test
+"""
+
+import argparse
+
+from datasets import load_dataset
+
+
+parser = argparse.ArgumentParser(
+    description="upload dataset to huggingface hub"
+)
+
+parser.add_argument("input_file", type=str, help="Input JSONL file containing text data.")
+parser.add_argument("repo_name", type=str, help="hugging face repository name")
+
+args = parser.parse_args()
+
+dataset = load_dataset("json", data_files=args.input_file, split="train")
+
+dataset.push_to_hub(args.repo_name)
